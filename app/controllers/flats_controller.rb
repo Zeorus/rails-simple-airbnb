90195e9dc3047e5d15b598f531271a3ccec49629
samuel schwob <samuelschwob@gmail.com>
2021-06-09 10:27:07 +0200

    optim CRUD

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -9,24 +9,42 @@ class FlatsController < ApplicationController
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @flat.destroy
+    redirect_to flats_path
   end
 
   private
 
   def set_flat
     @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :img)
   end
 
 end
